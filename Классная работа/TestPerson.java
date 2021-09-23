6880a195c49b1f3e1584332eb4c544ba90637a11
TerrifyingAnt <80172256+TerrifyingAnt@users.noreply.github.com>
2021-09-23 10:24:32 +0300

    Update TestPerson.java

--- Классная работа/TestPerson.java
+++ Классная работа/TestPerson.java
@@ -6,8 +6,14 @@ public class TestPerson{
         PersonException personExc = new PersonException();
 
         person.setName("Artyom");
+        if(person.getName == "")
+            System.out.println("It's impossible");
         person.setAge(19);
+        if(person.getAge > 120 || person.getAge < 0)
+            System.out.println("It's impossible");
         person.setHeight(1.9);
+        if(person.getHeight > 3.2 || person.getHeight < 0)
+            System.out.println("It's impossible");
         person.getName();
         person.getAge();
         person.getHeight();
